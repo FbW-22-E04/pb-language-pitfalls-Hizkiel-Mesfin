cfa684135321ab7b5d880cb8d6b75bc8b0870f98
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-07-05 12:34:13 +0200

    pythagoras or somehing, well if spelledn correctly

--- solution.js
+++ solution.js
@@ -1,1 +1,13 @@
+//#1
 
+// NaN stands for Not a Number
+
+//#1.1
+console.log("Q1.1:", isNaN("hello"), isNaN("3"), typeof NaN);
+
+//#2
+console.log((0.1 * 0.2).toFixed(2));
+
+//#3
+
+console.log(Infinity / 0, Infinity / Infinity, 1 / 0);
